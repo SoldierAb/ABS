e0bc53b3d79214b4bad594462bf439c5248ce631
SoldierAb <ab140140@163.com>
2018-05-08 23:11:29 +0800

    tea animation add

---
+++ xuecheng-1/src/components/simplecv/Simplecv.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+`;
+
+class Simplecv extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {}
+  }
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Wrapper>
+
+      </Wrapper>
+    );
+  }
+}
